96bf52efdbf09733db483fb7ced22925daa2898d
Michael <michael@click.com.br>
2025-05-28 18:55:36 -0400

    logo musa nextconfig

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
                 hostname: 'picsum.photos',
                 pathname: '/**',
             },
+            {
+                protocol: 'https',
+                hostname: 'musaclass.com.br',
+                pathname: '/**',
+            },
         ],
       },
 }
