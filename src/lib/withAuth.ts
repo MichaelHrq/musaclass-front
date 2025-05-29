ae04900f71ec935fe2028eb5276cb7e86df4098b
Michael <michael@click.com.br>
2025-05-28 23:10:47 -0400

    Refactor withAuth function to simplify authentication handling and remove unnecessary error logging

--- src/lib/withAuth.ts
+++ src/lib/withAuth.ts
@@ -1,18 +1,16 @@
-"use server";
-
-const withAuth =
-  <Args extends any[], Return>(action: (...args: Args) => Promise<Return>) =>
-  async (...args: Args): Promise<Return> => {
-    try {
-      console.log("With auth executing");
-      return await action(...args)
-    } catch (error: unknown) {
-      console.error("Authentication error:", error);
-
-      // await clearTokens();
-
-      throw new Error("Unknown authentication error");
-    }
+export default function withAuth(action: Function) {
+  return async function (...args: any[]) {
+    return await action(...args);
+    // try {
+    // console.log(`entrou no withauth`);
+    // await clearTokens();
+    // if (await isTokenExpired()) {
+    //   await refreshToken();
+    // }
+    // return await action(...args);
+    // } catch (error: any) {
+    // console.log(`with auth:`, Object.entries(error))
+    // await clearTokens();
+    // }
   };
-
-export default withAuth;
+}
