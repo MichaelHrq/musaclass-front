113b3e9f602b52b7eb723d6e17a265d43884b5e0
Michael <michael@click.com.br>
2025-05-28 22:54:58 -0400

    Refactor withAuth function to streamline error handling and remove unused token clearing logic

--- src/lib/withAuth.ts
+++ src/lib/withAuth.ts
@@ -1,28 +1,15 @@
-// withAuth.ts
 "use server";
-import { clearTokens } from "./authTokens";
 
 const withAuth =
   <Args extends any[], Return>(action: (...args: Args) => Promise<Return>) =>
   async (...args: Args): Promise<Return> => {
     try {
-      console.log("Auth middleware executing");
-
-      const result = await action(...args);
-      return result;
+      console.log("With auth executing");
+      return await action(...args)
     } catch (error: unknown) {
       console.error("Authentication error:", error);
 
-      // Garante que a limpeza de tokens seja uma Server Action
-      ("use server");
-      await clearTokens();
-
-      // if (error instanceof Error) {
-      //   if (error.message.includes("Unauthorized")) {
-      //     await redirectIfUnauthorized();
-      //   }
-      //   throw error;
-      // }
+      // await clearTokens();
 
       throw new Error("Unknown authentication error");
     }
