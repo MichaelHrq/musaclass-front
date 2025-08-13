338d56e46d329df0ddfc02d4a12ecc30e963bb79
Michael <michael@clickmulti.com.br>
2025-08-13 18:01:37 -0400

    fix dashboard

--- src/components/page/dashboard/anunciante/anuncios.tsx
+++ src/components/page/dashboard/anunciante/anuncios.tsx
@@ -6,10 +6,10 @@ import ButtonIcon from "@/components/ui/button/icon";
 import { Ellipsis } from "lucide-react";
 import Link from "next/link";
 
-export default function Anuncios({ items }: { items?: AnuncioType[] }) {
+export default function Anuncios({ items }: { items: AnuncioType[] }) {
   return (
     <>
-      {items ? (
+      {items?.length > 0 ? (
         items?.map((item) => (
           <div key={item.id} className="w-full relative">
             <ListAnuncios item={item} />
