c1d9b11a08ff88036b20134d6e05630b2c688ffd
Michael <michaelhrqfs@gmail.com>
2025-09-01 11:48:25 -0400

    next config url media

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,11 @@ const nextConfig: NextConfig = {
         hostname: "mcfilesdev.s3.amazonaws.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "s3.amazonaws.com",
+        pathname: "/**",
+      },
     ],
   },
 };
